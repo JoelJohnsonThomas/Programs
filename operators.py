88efc4fb7839cee8daf348c87f689952e4f22110
JoelJohnsonThomas <83132383+JoelJohnsonThomas@users.noreply.github.com>
2021-04-29 03:28:26 +0000

    Update and rename README.md to operators.py

---
+++ operators.py
@@ -0,0 +1,23 @@
+# Addition of two numbers
+a = int(input("enter the number:"))
+b = int(input("enter another number:"))
+c = a+b
+print("The addition of given two numbers is",c)
+        
+# Subtraction of two numbers
+x = int(input("enter the number:"))
+y = int(input("enter another number:"))
+z = x-y
+print("The subtraction of two given numbers is",z)
+
+#Multiplication of two numbers
+m = int(input("enter the number:"))
+n = int(input("enter another number:"))
+l = m*n
+print("The multipliction of two given numbers is",l)
+
+#Division of two numbers
+p = int(input("enter the number:"))
+q = int(input("enter another number:"))
+r = p/q
+print("The Division of two numbers is",r)
