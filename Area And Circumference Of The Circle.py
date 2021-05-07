003fbf0e712baa3b01d01ebbe83d725d91942b21
JoelJohnsonThomas <83132383+JoelJohnsonThomas@users.noreply.github.com>
2021-05-07 19:31:09 +0530

    Create Area And Circumference Of The Circle.py

---
+++ Area And Circumference Of The Circle.py
@@ -0,0 +1,9 @@
+rad = float(input("Enter The Radius Of The Circle : "))
+
+area = 3.14 * rad * rad
+
+circumference = 3.14 * 2 * rad
+
+print("The Area Of The Circle Is ", area, "sq.units")
+
+print("The Circumference Of The Circle Is ", circumference, "units")
