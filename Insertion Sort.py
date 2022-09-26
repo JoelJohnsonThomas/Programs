16aa857b1ea87cc55ee929b7dcf15af0265528ee
JoelJohnsonThomas <83132383+JoelJohnsonThomas@users.noreply.github.com>
2022-09-26 05:46:05 +0530

    Create Insertion Sort.py

---
+++ Insertion Sort.py
@@ -0,0 +1,18 @@
+def selectionsort(list):
+    for i in range(1,len(list)):
+        temp =list[i]
+        j =i-1
+        while( j>=0 and temp < list[j]):
+            list[j+1]=list[j]
+            j =j-1
+            list[j+1]=temp
+l=[]
+m=int(input('Enter The Number Of Elements To Be Added:'))
+for k in range(m):
+    e=int(input("Enter The Element To Be Added:"))
+    l.append(e)
+selectionsort(l)
+print(l)
+
+
+
